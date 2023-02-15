3e82e70e8462a4946425193ffaf921ad6fd61057
Valentin Chudinov <valentinchudinov@gmail.com>
2023-02-15 22:15:52 +0700

    last commit

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,15 +1,5 @@
 ﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-Console.WriteLine("\nPlease enter a three-digit number: ");
+Console.Write("Enter any number: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int thirdDigit = number % 100;
-
-if (number >= 100 && number <= 999)
-{
-    int secondDigit = thirdDigit % 10;
-    Console.WriteLine("The second digit is " + secondDigit);
-}
-else 
-{
-    Console.WriteLine("This number has no third digit.");
-}
+Console.Write(number > 99 ? number.ToString()[2] : $"There is no third digit in this number {number} ");
