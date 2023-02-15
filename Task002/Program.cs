9cffa5f3f6801b02d222e7bb2a50d1f2de3adb02
Valentin Chudinov <valentinchudinov@gmail.com>
2023-02-15 19:35:26 +0700

    last commit

---
+++ Task002/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Please enter a three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int thirdDigit = number % 100;
+
+if (number >= 100 && number <= 999)
+{
+    int secondDigit = thirdDigit % 10;
+    Console.WriteLine("The second digit is " + secondDigit);
+}
+else 
+{
+    Console.WriteLine("This number has no third digit.");
+}
---
+++ Task003/Program.cs
@@ -0,0 +1,41 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+
+Console.Write("Please enter a number that corresponds to a day of the week (1-7): ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day > 0 && day < 6)
+    Console.ForegroundColor = ConsoleColor.Yellow;
+else if (day == 6 || day == 7)
+    Console.ForegroundColor = ConsoleColor.Green;
+else
+    Console.ForegroundColor = ConsoleColor.Red;
+
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Tuesday is not a holiday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday is not a holiday");
+        break;
+    case 3:
+        Console.WriteLine("Tuesday is not a holiday");
+        break;
+    case 4:
+        Console.WriteLine("Tuesday is not a holiday");
+        break;
+    case 5:
+        Console.WriteLine("Tuesday is not a holiday");
+        break;
+    case 6:
+        Console.WriteLine("Tuesday is a holiday");
+        break;
+    case 7:
+        Console.WriteLine("Tuesday is a holiday");
+        break;
+    default:
+        Console.WriteLine("Invalid number");
+        break;
+}
+
+Console.ResetColor();
