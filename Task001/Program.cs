92784caeba6d133980303211cf370c82b35a1c97
Valentin Chudinov <valentinchudinov@gmail.com>
2023-02-15 19:34:17 +0700

    last commit

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int num;
+int secondNumber;
+
+Console.Write("Enter a three digit number: ");
+num = Convert.ToInt32(Console.ReadLine());
+secondNumber = (num / 10) % 10;
+
+Console.Write("\nSecond digit of the number is : " + secondNumber);
